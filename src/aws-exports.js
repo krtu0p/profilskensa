186d25daebc6ea7309e75f79cc9bfdc63013d86e
krtu0p <adiaksab99@gmail.com>
2025-02-12 19:01:05 +0800

    30%

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsExports = {
+        Auth: {
+            region: 'ap-southeast-1', // Ganti dengan region Cognito-mu
+            userPoolId: 'ap-southeast-1_2k824qi3Q', // Ganti dengan User Pool ID
+            userPoolWebClientId: '7g66hj4i3dth04u1tjbgks29nj', // Ganti dengan App Client ID
+        },
+}
+
+export default awsExports;
